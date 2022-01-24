46b9acb19d95bf98de1b782e7c2e1ac85ff1469e
ddodxy <alfariziridha014@gmail.com>
2022-01-25 00:12:04 +0800

    nah

--- HachiBot/modules/global_mute.py
+++ HachiBot/modules/global_mute.py
@@ -353,7 +353,7 @@ GMUTE_STATUS = CommandHandler(
     "gmutespam", gmutestat, pass_args=True, filters=Filters.chat_type.groups
 )
 
-GMUTE_ENFORCER = MessageHandler(filters=Filters.chat_type.groups, enforce_gmute)
+GMUTE_ENFORCER = MessageHandler(Filters.all & Filters.chat_type.groups, enforce_gmute)
 
 dispatcher.add_handler(GMUTE_HANDLER)
 dispatcher.add_handler(UNGMUTE_HANDLER)
