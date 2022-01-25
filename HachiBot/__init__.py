77734e03b764ba19cb11d7440ffc3609f40eef48
ddodxy <alfariziridha014@gmail.com>
2022-01-25 17:59:59 +0800

    piks

--- HachiBot/__init__.py
+++ HachiBot/__init__.py
@@ -99,6 +99,7 @@ if ENV:
     DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    MONGO_DB = os.environ.get("MONGO_DB", "HachiBot")
     ARQ_API = os.environ.get("ARQ_API", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
@@ -183,6 +184,7 @@ else:
     ERROR_LOGS = Config.ERROR_LOGS
     DB_URL = Config.SQLALCHEMY_DATABASE_URI
     MONGO_DB_URI = Config.MONGO_DB_URI
+    MONGO_DB = Config.MONGO_DB
     ARQ_API = Config.ARQ_API_KEY
     ARQ_API_URL = Config.ARQ_API_URL
     DONATION_LINK = Config.DONATION_LINK
