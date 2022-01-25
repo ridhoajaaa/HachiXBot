6a4567b5e2982e768d0bdb047ea5b80ea069f220
ddodxy <alfariziridha014@gmail.com>
2022-01-26 07:58:42 +0800

    img 2 pdf

--- HachiBot/__init__.py
+++ HachiBot/__init__.py
@@ -280,6 +280,8 @@ pgram = Client(
     bot_token=TOKEN,
 )
 
+app = Client("hachi", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+
 ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 try:
     ubot2.start()
