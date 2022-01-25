6771cbb2ba9dd20a3aa17595bdf9cba3f08a2ec7
ddodxy <alfariziridha014@gmail.com>
2022-01-25 13:32:56 +0800

    ..

--- HachiBot/modules/misc.py
+++ HachiBot/modules/misc.py
@@ -138,44 +138,6 @@ def wiki(update: Update, context: CallbackContext):
             )
 
 
-@register(pattern="^/wall ?(.*)")
-async def _(event):
-    try:
-        query = event.pattern_match.group(1)
-        feri = await event.reply("`Searching for Images What you're looking for.....`")
-        async with ubot.conversation("@AnosVoldigoadbot") as conv:
-            try:
-                query1 = await conv.send_message(f"/wall {query}")
-                r1 = await conv.get_response()
-                r2 = await conv.get_response()
-                await ubot.send_read_acknowledge(conv.chat_id)
-            except YouBlockedUserError:
-                return await feri.edit("`Emrorr lol`")
-            if r1.text.startswith("No"):
-                return await feri.edit(f"`Cannot find the image`")
-            img = await ubot.download_media(r1)
-            img2 = await ubot.download_media(r2)
-            await feri.edit("`Sending Image....`")
-            p = await tbot.send_file(
-                event.chat_id,
-                img,
-                force_document=False,
-                reply_to=event.reply_to_msg_id,
-            )
-            await tbot.send_file(
-                event.chat_id,
-                img2,
-                force_document=True,
-                reply_to=p,
-            )
-            await feri.delete()
-            await ubot.delete_messages(conv.chat_id, [r1.id, r2.id, query1.id])
-        await event.delete()
-        os.system("rm *.png *.jpg")
-    except TimeoutError:
-        return await feri.edit("`Cannot find the image`")
-
-
 __help__ = """
 *Available commands:*
 ❂ /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
--- HachiBot/modules/weather.py
+++ HachiBot/modules/weather.py
@@ -25,7 +25,6 @@ from telegram.error import BadRequest
 from HachiBot import dispatcher, API_WEATHER as APPID
 from HachiBot.modules.disable import DisableAbleCommandHandler
 from HachiBot.modules.helper_funcs.alternate import typing_action
-from HachiBot.events import register
 
 
 @typing_action
@@ -135,21 +134,7 @@ def weather(update, context):
             return
 
 
-@register(pattern="^/wttr (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-
-    sample_url = "https://wttr.in/{}.png"
-    # logger.info(sample_url)
-    input_str = event.pattern_match.group(1)
-    async with aiohttp.ClientSession() as session:
-        response_api_zero = await session.get(sample_url.format(input_str))
-        # logger.info(response_api_zero)
-        response_api = await response_api_zero.read()
-        with io.BytesIO(response_api) as out_file:
-            await event.reply(file=out_file)
-            
+__mod_name__ = "Weather"
 
 WEATHER_HANDLER = DisableAbleCommandHandler("weather", weather, pass_args=True)
 
