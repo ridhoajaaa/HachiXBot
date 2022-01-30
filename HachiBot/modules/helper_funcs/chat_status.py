b46e55fcf92f900e8bc1afc007f17cfc15c35878
ddodxy <alfariziridha014@gmail.com>
2022-01-30 12:01:35 +0800

    ,

--- HachiBot/modules/helper_funcs/chat_status.py
+++ HachiBot/modules/helper_funcs/chat_status.py
@@ -16,8 +16,8 @@ from HachiBot import (
 from telegram import Chat, ChatMember, ParseMode, Update
 from telegram.ext import CallbackContext
 
-# stores admemes in memory for 10 min.
-ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10, timer=perf_counter)
+# refresh cache 10m
+ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10)
 THREAD_LOCK = RLock()
 
 
@@ -36,12 +36,12 @@ def is_sudo_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
         chat.type == "private"
-        or user_id in DRAGONS
         or user_id in DEV_USERS
+        or user_id in {777000, 1087968824}
         or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
-    ):  # Count telegram and Group Anonymous as admin
+    ):
         return True
+
     if not member:
         with THREAD_LOCK:
             # try to fetch from cache first.
@@ -51,14 +51,15 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
                 # keyerror happend means cache is deleted,
                 # so query bot api again and return user status
                 # while saving it in cache for future useage...
-                chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
-                admin_list = [x.user.id for x in chat_admins]
-                ADMIN_CACHE[chat.id] = admin_list
+                try:
+                    chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
+                    admin_list = [x.user.id for x in chat_admins]
+                    ADMIN_CACHE[chat.id] = admin_list
 
-                return user_id in admin_list
-    else:
-        return member.status in ("administrator", "creator")
-
+                    if user_id in admin_list:
+                        return True
+                except Unauthorized:
+                    return False
 
 def is_bot_admin(chat: Chat, bot_id: int, bot_member: ChatMember = None) -> bool:
     if chat.type == "private" or chat.all_members_are_administrators:
@@ -74,24 +75,18 @@ def can_delete(chat: Chat, bot_id: int) -> bool:
     return chat.get_member(bot_id).can_delete_messages
 
 
-def is_user_ban_protected(update: Update, user_id: int, member: ChatMember = None) -> bool:
-    chat = update.effective_chat
-    msg = update.effective_message
+def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
-            chat.type == "private"
-            or user_id in SUDO_USERS
-            or user_id in DEV_USERS
-            or user_id in WHITELIST_USERS
-            or user_id in SARDEGNA_USERS
-            or chat.all_members_are_administrators
-            or (msg.reply_to_message and msg.reply_to_message.sender_chat is not None
-                and msg.reply_to_message.sender_chat.type != "channel")
+        chat.type == "private"
+        or user_id in DEV_USERS
+        or user_id in WHITELIST_USERS
+        or chat.all_members_are_administrators
+        or user_id in {777000, 1087968824}
     ):
         return True
 
     if not member:
         member = chat.get_member(user_id)
-
     return member.status in ("administrator", "creator")
 
 
